aacc82b46a0a8fa1a5b50b23c7e0c11afcbd7b4c
Vadim <soinvadikv@mail.ru>
2023-01-09 19:01:51 +0400

    РЕШИЛ ЗАДАНИЕ 4

---
+++ zadaniy04_semenar01/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int numbers = Convert.ToInt32 (Console.ReadLine ());
+int i = 1;
+while ( i <= numbers )
+{
+    if (i % 2 == 0) Console.Write (i + " ");
+    i++;
+}
+Console.WriteLine("");
