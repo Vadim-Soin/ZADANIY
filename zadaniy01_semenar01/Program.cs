9311600fd85c98c26c36d568b46cf13fa6f0f32c
Vadim <soinvadikv@mail.ru>
2023-01-09 18:50:56 +0400

    РЕШИЛ ЗАДАЧУ 1

---
+++ zadaniy01_semenar01/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int numbers1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int numbers2 = Convert.ToInt32(Console.ReadLine());
+if (numbers1 > numbers2)
+    Console.WriteLine(" Первое число больше "+numbers1);
+if (numbers2 > numbers1)    
+    Console.WriteLine(" Второе число больше "+numbers2);
+if (numbers1 == numbers2)
+    Console.WriteLine(" Числа равны " +numbers1, numbers2);
