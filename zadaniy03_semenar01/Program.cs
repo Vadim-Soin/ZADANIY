78b6f10f2af275f54c8db8fd855b9f5e6ee6a580
Vadim <soinvadikv@mail.ru>
2023-01-09 18:59:37 +0400

    РЕШИЛ ЗАДАНИЕ 3

---
+++ zadaniy03_semenar01/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine ("Введите число");
+int numbers1 = Convert.ToInt32(Console.ReadLine());
+if (numbers1 % 2 == 0 ) Console.WriteLine ("Введенное число четное " +numbers1);
+else Console.WriteLine ("Введенное число не четное " +numbers1);
